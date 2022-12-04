28c9017d6cd91fcb545b52d2840a9bcb1896883b
Yifan Chen <chenyifan003@gmail.com>
2022-12-04 14:54:22 +0800

    updata

--- R/robust_cvx_biclustr.R
+++ R/robust_cvx_biclustr.R
@@ -57,17 +57,32 @@
 #' image(a$U,zlim=c(-4,4))
 
 
-
-
-
-robust_cvx_biclustr <- function(X,W1=NULL,W2=NULL,V1=NULL,V2=NULL,Y1=NULL,Y2=NULL,Z1=NULL,Z2=NULL,
-                                max_iter=700, rho,lambda,wt_row,wt_col,tol_abs=1e-05){
+rcbc <- function(X,tuning_free=c("yes","no"),lambda,k_row=10,k_col=10,tau=NULL,rho=0.35,
+                 W1=NULL,W2=NULL,V1=NULL,V2=NULL,Y1=NULL,Y2=NULL,Z1=NULL,Z2=NULL,
+                 max_iter=700,tol_abs=1e-05){
+  tuning_free <- match.arg(tuning_free)
+  if ((tuning_free == "yes") & (is.null(tau) == FALSE)){
+    warning("The input tau will not be used since you are choosing tuning-free method.")
+  }
+  if ((tuning_free == "no") & (is.null(tau) == TRUE)){
+    stop("Error: You are choosing the non-tuning-free method while not specifying a tau.")
+  }
   n <- as.integer(nrow(X))
   p <- as.integer(ncol(X))
   E1 <- create_E_matrix(n)$E
   E2 <- create_E_matrix(p)$E
   nK1 <- nrow(E1)
   nK2 <- nrow(E2)
+
+  X <- X - mean(X[X>quantile(X,.05) & X<quantile(X,.95)])
+  X <- X/norm(X,'f')
+  scal <- 17 / (quantile(X,.95)-quantile(X,.05))
+  X <- X*scal
+
+  wt_row <- robust_weights(X,1.345*mad(X),0.001,k_row)
+  wt_col <- robust_weights(t(X),1.345*mad(X),0.001,k_col)
+
+
   if(is.null(V1)==TRUE){
     V1 <- matrix(1,nrow=nK1,ncol=p)
     #V <- matrix(rnorm(nK*p),nrow=nK,ncol=p)
@@ -100,56 +115,20 @@ robust_cvx_biclustr <- function(X,W1=NULL,W2=NULL,V1=NULL,V2=NULL,Y1=NULL,Y2=NUL
     Z2 <- matrix(0,nrow=p,ncol=n)
     #Z <- matrix(rnorm(n*p),nrow=n,ncol=p)
   }
-  sol = robust_convex_bicluster(X=X,W1=W1,W2=W2,V1=V1,V2=V2,Y1=Y1,Y2=Y2,Z1=Z1,Z2=Z2,
-                                E1=E1,E2=E2,max_iter=max_iter,tol_abs=tol_abs,
-                                lambda=lambda,rho=rho,wt_row=wt_row,wt_col=wt_col)
-  return(list(U=sol$U,V_row=sol$V_row,V_col=sol$V_col,iter=sol$iter,tol=sol$tol,tau=sol$tau))
+  if (tuning_free == "yes"){
+    sol <- robust_convex_bicluster(X=X,W1=W1,W2=W2,V1=V1,V2=V2,Y1=Y1,Y2=Y2,Z1=Z1,Z2=Z2,
+                                   E1=E1,E2=E2,max_iter=max_iter,tol_abs=tol_abs,
+                                   lambda=lambda,rho=rho,wt_row=wt_row,wt_col=wt_col)
+    return(list(U=sol$U,V_row=sol$V_row,V_col=sol$V_col,iter=sol$iter,tol=sol$tol,tau=sol$tau))
+  }
+  if (tuning_free == "no"){
+    sol <-  robust_convex_bicluster_naive(X=X,W1=W1,W2=W2,V1=V1,V2=V2,Y1=Y1,Y2=Y2,Z1=Z1,Z2=Z2,
+                                        E1=E1,E2=E2,max_iter=max_iter,tol_abs=tol_abs,
+                                        lambda=lambda,rho=rho,tau=tau,wt_row=wt_row,wt_col=wt_col)
+    return(list(U=sol$U,V_row=sol$V_row,V_col=sol$V_col,iter=sol$iter,tol=sol$tol))
+  }
 }
 
 
 
-robust_cvx_biclustr_naive <- function(X,W1=NULL,W2=NULL,V1=NULL,V2=NULL,Y1=NULL,Y2=NULL,Z1=NULL,Z2=NULL,
-                                      max_iter=700,rho,tau,lambda,wt_row,wt_col,tol_abs=1e-05){
-  n <- as.integer(nrow(X))
-  p <- as.integer(ncol(X))
-  E1 <- create_E_matrix(n)$E
-  E2 <- create_E_matrix(p)$E
-  nK1 <- nrow(E1)
-  nK2 <- nrow(E2)
-  if(is.null(V1)==TRUE){
-    V1 <- matrix(1,nrow=nK1,ncol=p)
-    #V <- matrix(rnorm(nK*p),nrow=nK,ncol=p)
-  }
-  if(is.null(W1)==TRUE){
-    W1 <- matrix(1,nrow=n,ncol=p)
-    #W <- matrix(rnorm(n*p),nrow=n,ncol=p)
-  }
-  if(is.null(Y1)==TRUE){
-    Y1 <- matrix(0,nrow=nK1,ncol=p)
-    #Y <- matrix(rnorm(nK*p),nrow=nK,ncol=p)
-  }
-  if(is.null(Z1)==TRUE){
-    Z1 <- matrix(0,nrow=n,ncol=p)
-    #Z <- matrix(rnorm(n*p),nrow=n,ncol=p)
-  }
-  if(is.null(V2)==TRUE){
-    V2 <- matrix(1,nrow=nK2,ncol=n)
-    #V <- matrix(rnorm(nK*p),nrow=nK,ncol=p)
-  }
-  if(is.null(W2)==TRUE){
-    W2 <- matrix(1,nrow=p,ncol=n)
-    #W <- matrix(rnorm(n*p),nrow=n,ncol=p)
-  }
-  if(is.null(Y2)==TRUE){
-    Y2 <- matrix(0,nrow=nK2,ncol=n)
-    #Y <- matrix(rnorm(nK*p),nrow=nK,ncol=p)
-  }
-  if(is.null(Z2)==TRUE){
-    Z2 <- matrix(0,nrow=p,ncol=n)
-    #Z <- matrix(rnorm(n*p),nrow=n,ncol=p)
-  }
-  sol = robust_convex_bicluster_naive(X=X,W1=W1,W2=W2,V1=V1,V2=V2,Y1=Y1,Y2=Y2,Z1=Z1,Z2=Z2,
-                                      E1=E1,E2=E2,max_iter=max_iter,tol_abs=tol_abs,
-                                      lambda=lambda,rho=rho,tau=tau,wt_row=wt_row,wt_col=wt_col)
-  return(list(U=sol$U,V_row=sol$V_row,V_col=sol$V_col,iter=sol$iter,tol=sol$tol))
-}
+
